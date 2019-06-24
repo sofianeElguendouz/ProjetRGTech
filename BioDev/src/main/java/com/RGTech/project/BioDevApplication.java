b81e979ea2c97662f9c6039eb17a7b9cdbdfd66c
Sofiane Elguendouze <fs_elguendouze@esi.dz>
2019-06-24 14:49:06 +0100

    first

---
+++ BioDev/src/main/java/com/RGTech/project/BioDevApplication.java
@@ -0,0 +1,13 @@
+package com.RGTech.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioDevApplication.class, args);
+	}
+
+}
